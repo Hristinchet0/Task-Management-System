6ef21c6c80623f545b3256c26c1793859aea5a09
Hristinchet0 <bansko.sport@gmail.com>
2023-06-11 17:54:46 +0300

    Add

---
+++ src/main/java/com/taskmanagementsystem/tms/Entity/ExportForm.java
@@ -0,0 +1,13 @@
+package com.taskmanagementsystem.tms.Entity;
+
+public class ExportForm {
+    private String format;
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+}
---
+++ src/main/java/com/taskmanagementsystem/tms/enums/Progress.java
@@ -0,0 +1,17 @@
+//package com.taskmanagementsystem.tms.enums;
+//
+//public enum Progress {
+//    NOT_STARTED("Not Started"),
+//    IN_PROGRESS("In Progress"),
+//    COMPLETED("Completed");
+//
+//    private String displayName;
+//
+//    Progress(String displayName) {
+//        this.displayName = displayName;
+//    }
+//
+//    public String getDisplayName() {
+//        return displayName;
+//    }
+//}
