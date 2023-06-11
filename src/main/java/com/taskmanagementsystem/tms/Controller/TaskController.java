3a51d274746604db68a95b64a763f5f7aac184ef
Hristinchet0 <bansko.sport@gmail.com>
2023-06-11 22:35:40 +0300

    Update

--- src/main/java/com/taskmanagementsystem/tms/Controller/TaskController.java
+++ src/main/java/com/taskmanagementsystem/tms/Controller/TaskController.java
@@ -2,17 +2,26 @@ package com.taskmanagementsystem.tms.Controller;
 
 import com.taskmanagementsystem.tms.Entity.Task;
 import com.taskmanagementsystem.tms.Service.TaskService;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class TaskController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
     @Autowired
     private TaskService taskService;
@@ -23,7 +32,6 @@ public class TaskController {
         model.addAttribute("task", tasks);
         return "index";
     }
-
 
     @GetMapping("/addTask")
     public String addTask() {
@@ -59,72 +67,25 @@ public class TaskController {
 
     @PostMapping("/export")
     public String exportTasks(@RequestParam("files") String format) {
-
+        logger.info("Exporting tasks in file in format: {}", format);
         List<Task> tasks = taskService.getTasks();
 
         try {
             if ("CSV".equalsIgnoreCase(format)) {
-                exportToCSV(tasks);
+                taskService.exportToCSV(tasks);
             } else if ("JSON".equalsIgnoreCase(format)) {
-                exportToJSON(tasks);
+                taskService.exportToJSON(tasks);
             } else if ("TXT".equalsIgnoreCase(format)) {
-                exportToTXT(tasks);
+                taskService.exportToTXT(tasks);
             } else if ("EXCEL".equalsIgnoreCase(format)) {
-                //
+                taskService.exportToEXCEL(tasks);
             } else {
                 throw new IllegalArgumentException("Invalid export format: " + format);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("Error exporting tasks in file: {}, {}", e.getMessage(), e.getCause());
         }
 
         return "redirect:/";
     }
-
-    private void exportToCSV(List<Task> tasks) throws IOException {
-        String csvFile = "tasks.csv";
-        try (FileWriter writer = new FileWriter(csvFile)) {
-
-            writer.write("Title,Description,Completed\n");
-
-            for (Task task : tasks) {
-                writer.write(String.format("%s,%s,%s\n", task.getTitle(), task.getDescription(), task.getProgress()));
-            }
-        }
-    }
-
-    private void exportToJSON(List<Task> tasks) throws IOException {
-        String jsonFile = "tasks.json";
-        try (FileWriter writer = new FileWriter(jsonFile)) {
-            writer.write("[\n");
-
-            for (int i = 0; i < tasks.size(); i++) {
-                Task task = tasks.get(i);
-                writer.write(String.format("  {\n    \"title\": \"%s\",\n    \"description\": \"%s\",\n    \"completed\": %s\n  }",
-                        task.getTitle(), task.getDescription(), task.getProgress()));
-                if (i < tasks.size() - 1) {
-                    writer.write(",");
-                }
-                writer.write("\n");
-            }
-
-            writer.write("]\n");
-        }
-    }
-
-    private void exportToTXT(List<Task> tasks) throws IOException {
-        String txtFile = "tasks.txt";
-        try (FileWriter writer = new FileWriter(txtFile)) {
-
-            for (Task task : tasks) {
-                writer.write(String.format("Title: %s\n", task.getTitle()));
-                writer.write(String.format("Description: %s\n", task.getDescription()));
-                writer.write(String.format("Completed: %s\n", task.getProgress()));
-                writer.write("----------------------\n");
-            }
-        }
-    }
-
-
-
 }
--- src/main/java/com/taskmanagementsystem/tms/Service/TaskService.java
+++ src/main/java/com/taskmanagementsystem/tms/Service/TaskService.java
@@ -2,14 +2,25 @@ package com.taskmanagementsystem.tms.Service;
 
 import com.taskmanagementsystem.tms.Entity.Task;
 import com.taskmanagementsystem.tms.Repository.TaskRepository;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class TaskService {
+
+    private static final String FILE_PATH = "tasks.xlsx";
     @Autowired
     private TaskRepository taskRepository;
 
@@ -29,4 +40,52 @@ public class TaskService {
     public void deleteTaskById(Long id) {
         taskRepository.deleteById(Math.toIntExact(id));
     }
+
+    public void exportToCSV(List<Task> tasks) throws IOException {
+        String csvFile = "tasks.csv";
+        try (FileWriter writer = new FileWriter(csvFile)) {
+
+            writer.write("Title,Description,Completed\n");
+
+            for (Task task : tasks) {
+                writer.write(String.format("%s,%s,%s\n", task.getTitle(), task.getDescription(), task.getProgress()));
+            }
+        }
+    }
+
+    public void exportToJSON(List<Task> tasks) throws IOException {
+        String jsonFile = "tasks.json";
+        try (FileWriter writer = new FileWriter(jsonFile)) {
+            writer.write("[\n");
+
+            for (int i = 0; i < tasks.size(); i++) {
+                Task task = tasks.get(i);
+                writer.write(String.format("  {\n    \"title\": \"%s\",\n    \"description\": \"%s\",\n    \"progress\": %s\n  }",
+                        task.getTitle(), task.getDescription(), task.getProgress()));
+                if (i < tasks.size() - 1) {
+                    writer.write(",");
+                }
+                writer.write("\n");
+            }
+
+            writer.write("]\n");
+        }
+    }
+
+    public void exportToTXT(List<Task> tasks) throws IOException {
+        String txtFile = "tasks.txt";
+        try (FileWriter writer = new FileWriter(txtFile)) {
+
+            for (Task task : tasks) {
+                writer.write(String.format("Title: %s\n", task.getTitle()));
+                writer.write(String.format("Description: %s\n", task.getDescription()));
+                writer.write(String.format("Completed: %s\n", task.getProgress()));
+                writer.write("----------------------\n");
+            }
+        }
+    }
+
+
+
+
 }
